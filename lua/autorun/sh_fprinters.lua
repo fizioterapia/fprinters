329b95e29312be89281c9058f009d409195e4c0d
fizioterapia <cubasa4@gmail.com>
2022-08-06 00:33:31 +0200

    remove unused file

--- lua/autorun/sh_fprinters.lua
+++ lua/autorun/sh_fprinters.lua
@@ -7,12 +7,10 @@ if SERVER then
     AddCSLuaFile("fprinters/config/sh_config.lua")
     AddCSLuaFile("fprinters/printers/sh_printers.lua")
     AddCSLuaFile("fprinters/vgui/cl_fonts.lua")
-    AddCSLuaFile("fprinters/vgui/cl_printers_admin.lua")
     AddCSLuaFile("fprinters/vgui/cl_printer_menu.lua")
 else
     include("fprinters/config/sh_config.lua")
     include("fprinters/printers/sh_printers.lua")
     include("fprinters/vgui/cl_fonts.lua")
-    include("fprinters/vgui/cl_printers_admin.lua")
     include("fprinters/vgui/cl_printer_menu.lua")
 end
